70e446f8a244abbac84a89815a7dbb6f566502ae
MatteyGG <matgaranin@gmail.com>
2025-02-14 08:11:07 +0300

    modified:   app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,11 @@ import Script from "next/script";
 
 export default function Home() {
   return (
+    <div className="w-screen h-screen bg-black">
     <div>
       <canvas id="heart"></canvas>
       <Script src="heart.js" strategy="beforeInteractive" />
+    </div>
     </div>
   );
 }
